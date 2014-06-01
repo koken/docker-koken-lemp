c78089d9702a10b02a4c62f8b894b3a60b17d3da
Brad Daily <brad@bradleyboy.com>
2014-06-01 00:56:45 -0400

    Cleanup

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-CHECK=`docker ps -a | grep $1-storage | wc -l`
+CHECK=$(docker ps -a | grep $1-storage | wc -l)
 
 if [ "$CHECK" -ne 1 ]; then
 	echo "No storage container found. Creating..."
 	docker run -v /var/lib/mysql -v /usr/share/nginx/www --name $1-storage busybox true
 fi;
 
-APP_CHECK=`docker ps | grep $1-application | cut -c -12`
+APP_CHECK=$(docker ps | grep $1-application | cut -c -12)
 
 if [ "$APP_CHECK" != "" ]; then
 	echo "Existing app container found, restarting."
-	docker stop $APP_CHECK
-	docker rm $APP_CHECK
+	ID=$(docker stop $APP_CHECK)
+	ID=$(docker rm $APP_CHECK)
 fi;
 
-docker run -dt --volumes-from $1-storage -p 8888:8888 --name $1-application -h $1 -e VIRTUAL_HOST=$1 bradleyboy/docker-koken-nginx
-echo "Koken container started."
+CID=$(docker run -dt --volumes-from $1-storage -p 8888:8888 --name $1-application -h $1 -e VIRTUAL_HOST=$1 bradleyboy/docker-koken-nginx)
+echo "Koken container started: $CID"
